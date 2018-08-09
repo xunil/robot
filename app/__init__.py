d748330ddfda2f331d062d8d02ca99d9ae5c87d2
Robert Liesenfeld <xunil@xunil.net>
2018-08-08 17:44:04 -0700

    Succeeded in getting the UI basically where I want it.  Unfortunately Flask app contexts don't work the way I'd hoped, and I'll have to store state outside the Flask app.  This motivates pulling the MIDI server component out to a separate process.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+LIVE_PLAY = 'live_play'
+RECORDING = 'record'
+JUKEBOX = 'jukebox'
+play_mode = LIVE_PLAY
+
 from app import routes
-#from app.midiloop import activate_midiloop
 from app import midiloop
+
+bootstrap = Bootstrap(app)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,32 @@
+from flask import render_template, jsonify
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
+
+@app.route('/live_play', methods=['POST'])
+def live_play():
+    # XXX: play_mode is function local.
+    play_mode = LIVE_PLAY
+    return jsonify({'status': 'OK'})
+
+@app.route('/start_record', methods=['POST'])
+def start_record():
+    play_mode = RECORDING
+    return jsonify({'status': 'OK'})
+
+@app.route('/stop_record', methods=['POST'])
+def stop_record():
+    play_mode = LIVE_PLAY
+    return jsonify({'status': 'OK'})
+
+@app.route('/jukebox', methods=['POST'])
+def jukebox():
+    play_mode = JUKEBOX
+    return jsonify({'status': 'OK'})
+
+@app.route('/mode', methods=['GET'])
+def current_mode():
+    return jsonify({'mode': play_mode})
